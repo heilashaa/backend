86962c23869ff3d31a9dd751a0848e0da1f8ceea
iheilash <iryna.heilash@ontravelsolutions.com>
2020-01-28 13:49:47 +0300

    initial

---
+++ src/main/java/com/haapp/formicary/FormicaryApplication.java
@@ -0,0 +1,13 @@
+package com.haapp.formicary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormicaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormicaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haapp/formicary/FormicaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haapp.formicary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormicaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
